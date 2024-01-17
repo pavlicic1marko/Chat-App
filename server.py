3d9b41281101a2ead75694d21b9334caa994ff4b
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-01-17 13:06:16 +0100

    creating server

---
+++ server.py
@@ -0,0 +1,14 @@
+import socket
+
+HOST_NAME = socket.gethostname()  # get  host name of your computer in local network
+print(HOST_NAME)
+PORT = 12345
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # INET is ipv4, SOCK_STREAM is TCP
+s.bind((HOST_NAME, PORT))
+s.listen(4)  # backlog , number of unaccepted connections
+
+while True:
+    client, address = s.accept()
+    print('Client is connected and hase the address: ' + address)
+
