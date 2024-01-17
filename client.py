fbf5ed57ecfe911075579c0b16d04912ddf34c65
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-01-17 14:45:52 +0100

    testing buffer size

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 import socket
 
 HOST_NAME = socket.gethostname()  # get  host name of your computer in local network
-PORT = 12345
+PORT = 12344
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # INET is ipv4, SOCK_STREAM is TCP
 s.connect((HOST_NAME, PORT))
-msg = s.recv(100)  # buffer size
-print(msg.decode("utf-8"))  # decode using utf-8 encoding
-
+msg1 = s.recv(10)  # buffer size, argument is the number of bytes
+print(msg1.decode("utf-8"))  # decode using utf-8 encoding
+msg2 = s.recv(10)
+print(msg2.decode("utf-8"))
